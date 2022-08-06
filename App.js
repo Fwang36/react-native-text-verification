fcb1a23f0b423a5a62a8c7811c2dd3ae1dd4c5c9
Francis <fwang36@gmail.com>
2022-08-06 14:24:23 -0400

    add dependencies and create test sign up page

--- App.js
+++ App.js
@@ -2,13 +2,88 @@ import { StatusBar } from 'expo-status-bar';
 import styles from './App.style';
 import { React, useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import PhoneInput from "react-native-phone-input";
+import { Form, FormItem } from 'react-native-form-component';
+import {
+  CodeField,
+  Cursor,
+  useBlurOnFulfill,
+  useClearByFocusCell,
+} from 'react-native-confirmation-code-field';
 
 export default function App() {
+
+  const [phoneNumber, setPhoneNumber] = useState("");
+
+  const CELL_COUNT = 4;
+  const [value, setValue] = useState('');
+  const ref = useBlurOnFulfill({value, cellCount: CELL_COUNT});
+  const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+    value,
+    setValue,
+  });
+
   return (
     <View style={styles.formContainer}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+        <Text style={styles.title}>
+          Sign up
+        </Text>
+
+        <Form 
+          // onButtonPress={changeSelect}
+          buttonStyle={styles.submitBtn}
+          buttonText="Verify"  
+        >
+          <View style={styles.labelContainer}>
+            <Text style={styles.label}>
+              Phone number
+            </Text>
+          </View>
+
+          <PhoneInput
+            style={styles.phoneInput}
+            value={phoneNumber}
+            initialCountry="us"
+            onChangePhoneNumber={(text) => {
+              setPhoneNumber(text);
+            }}
+            withShadow
+            autoFocus
+           />
+
+          <View style={styles.labelContainer}>
+            <Text style={styles.label}>
+              Verify number
+            </Text>
+          </View>
+
+          <View style={styles.verifyContainer}>
+            <CodeField
+              ref={ref}
+              {...props}
+              // Use `caretHidden={false}` when users can't paste a text value, because context menu doesn't appear
+              value={value}
+              onChangeText={setValue}
+              cellCount={CELL_COUNT}
+              rootStyle={styles.codeFieldRoot}
+              keyboardType="number-pad"
+              textContentType="oneTimeCode"
+              renderCell={({index, symbol, isFocused}) => (
+                <Text
+                  key={index}
+                  style={[styles.cell, isFocused && styles.focusCell]}
+                  onLayout={getCellOnLayoutHandler(index)}>
+                  {symbol || (isFocused ? <Cursor /> : null)}
+                </Text>
+              )}
+            />
+          </View>
+
+        </Form>
+
+      </View>
+    
   );
 }
 
